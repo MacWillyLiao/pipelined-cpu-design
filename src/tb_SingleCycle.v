49cdb4bad00d7d02ece29141cda3c8550eb5b924
MacWillyLiao <116270282+MacWillyLiao@users.noreply.github.com>
2026-01-17 12:03:36 +0800

    Update tb_SingleCycle.v

--- src/tb_SingleCycle.v
+++ src/tb_SingleCycle.v
@@ -1,7 +1,7 @@
 `timescale 1ns/ 1ns
-module tb_SingleCycle();
-	reg clk, rst;
+module tb_SingleCycle ();
 	
+	reg clk, rst;
 	initial begin
 		clk = 1;
 		forever #5 clk = ~clk;
@@ -9,33 +9,33 @@ module tb_SingleCycle();
 
 	initial begin
 		rst = 1'b1;
-		$readmemh("instr_mem.txt", CPU.InstrMem.mem_array );
-		$readmemh("data_mem.txt", CPU.DatMem.mem_array );
-		$readmemh("reg.txt", CPU.RegFile.file_array );
+		$readmemh("instr_mem.txt", CPU.InstrMem.mem_array);
+		$readmemh("data_mem.txt", CPU.DatMem.mem_array);
+		$readmemh("reg.txt", CPU.RegFile.file_array);
 		#10;
 		rst = 1'b0;
 	end
 	
-	always @( posedge clk ) begin
-		$display( "%d, PC:", $time/10-1, CPU.pc );
-		if ( CPU.opcode == 6'd0 ) begin
-			$display( "%d, wd: %d", $time/10-1, CPU.rfile_wd );
-			if ( CPU.funct_1 == 6'd32 ) $display( "%d, ADD\n", $time/10-1 );
-			else if ( CPU.funct_1 == 6'd34 ) $display( "%d, SUB\n", $time/10-1 );
-			else if ( CPU.funct_1 == 6'd36 ) $display( "%d, AND\n", $time/10-1 );
-			else if ( CPU.funct_1 == 6'd37 ) $display( "%d, OR\n", $time/10-1 );
-			else if ( CPU.funct_1 == 6'd42 ) $display( "%d, SLT\n", $time/10-1 );
-			else if ( CPU.funct_1 == 6'd8 ) $display( "%d, JR\n", $time/10-1 );
-			else if ( CPU.funct_1 == 6'd25 ) $display( "%d, MULTU\n", $time/10-1 );
-			else if ( CPU.funct_1 == 6'd16 ) $display( "%d, MFHI\n", $time/10-1 );
-			else if ( CPU.funct_1 == 6'd18 ) $display( "%d, MFLO\n", $time/10-1 );
-			else if ( CPU.funct_1 == 6'd0 ) $display( "%d, NOP\n", $time/10-1 );
+	always @(posedge clk) begin
+		$display("%d, PC:", $time/10-1, CPU.pc);
+		if (CPU.opcode == 6'd0) begin
+			$display("%d, wd: %d", $time/10-1, CPU.rfile_wd);
+			if (CPU.funct_1 == 6'd32 ) $display("%d, ADD\n", $time/10-1);
+			else if (CPU.funct_1 == 6'd34) $display("%d, SUB\n", $time/10-1);
+			else if (CPU.funct_1 == 6'd36) $display("%d, AND\n", $time/10-1);
+			else if (CPU.funct_1 == 6'd37) $display("%d, OR\n", $time/10-1);
+			else if (CPU.funct_1 == 6'd42) $display("%d, SLT\n", $time/10-1);
+			else if (CPU.funct_1 == 6'd8) $display("%d, JR\n", $time/10-1);
+			else if (CPU.funct_1 == 6'd25) $display("%d, MULTU\n", $time/10-1);
+			else if (CPU.funct_1 == 6'd16) $display("%d, MFHI\n", $time/10-1);
+			else if (CPU.funct_1 == 6'd18) $display("%d, MFLO\n", $time/10-1);
+			else if (CPU.funct_1 == 6'd0) $display("%d, NOP\n", $time/10-1);
 		end
-		else if ( CPU.opcode == 6'd35 ) $display( "%d, LW\n", $time/10-1 );
-		else if ( CPU.opcode == 6'd43 ) $display( "%d, SW\n", $time/10-1 );
-		else if ( CPU.opcode == 6'd4 ) $display( "%d, BEQ\n", $time/10-1 );
-		else if ( CPU.opcode == 6'd2 ) $display( "%d, J\n", $time/10-1 );
-		else if ( CPU.opcode == 6'd12 ) $display( "%d, ANDI\n", $time/10-1 );
+		else if (CPU.opcode == 6'd35) $display("%d, LW\n", $time/10-1);
+		else if (CPU.opcode == 6'd43) $display("%d, SW\n", $time/10-1);
+		else if (CPU.opcode == 6'd4) $display("%d, BEQ\n", $time/10-1);
+		else if (CPU.opcode == 6'd2) $display("%d, J\n", $time/10-1);
+		else if (CPU.opcode == 6'd12) $display("%d, ANDI\n", $time/10-1);
 	end
 	
 	initial begin
@@ -43,6 +43,6 @@ module tb_SingleCycle();
         $stop;
     end
 	
-	mips_single CPU( clk, rst );
+	mips_single CPU (clk, rst);
 	
 endmodule
