66bb8b1fd646344372829e1e48613f36fb84a044
MacWillyLiao <116270282+MacWillyLiao@users.noreply.github.com>
2026-01-17 12:45:01 +0800

    Update FA.v

--- alu-design/src/FA.v
+++ alu-design/src/FA.v
@@ -1,13 +1,13 @@
-module FA( a,b,cin,cout,sum);
+module FA (a, b, cin, cout, sum);
   
-  input a,b,cin;
-  output sum,cout;
-  wire x,y,z;
-  
-  xor( x,a,b );
-  xor(sum,x,cin);
-  and(y,a,b);
-  and(z,x,cin);
-  or(cout,y,z);
+    input a, b, cin;
+    output sum, cout;
+    wire x, y, z;
+    
+    xor (x, a, b);
+    xor (sum, x, cin);
+    and (y, a, b);
+    and (z, x, cin);
+    or (cout, y, z);
   
 endmodule
