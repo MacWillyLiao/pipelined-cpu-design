1c8a0ea096bf68b409da5c0e5a1f1ebabb810190
MacWillyLiao <116270282+MacWillyLiao@users.noreply.github.com>
2026-01-17 11:50:19 +0800

    Update mux2.v

--- src/mux2.v
+++ src/mux2.v
@@ -1,7 +1,7 @@
-module mux2( sel, a, b, y );
-    parameter bitwidth=32;
+module mux2 (sel, a, b, y);
+    parameter bitwidth = 32;
     input sel;
-    input  [bitwidth-1:0] a, b;
+    input [bitwidth-1:0] a, b;
     output [bitwidth-1:0] y;
 
     assign y = sel ? b : a;
