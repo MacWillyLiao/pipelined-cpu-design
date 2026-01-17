bb8e1add08519bc20a307047c82c34f92a007c97
MacWillyLiao <116270282+MacWillyLiao@users.noreply.github.com>
2026-01-17 10:47:35 +0800

    Update ID_EX.v

--- src/ID_EX.v
+++ src/ID_EX.v
@@ -1,13 +1,13 @@
-module  ID_EX ( reset, clk, RD1_in, RD1_out, RD2_in, RD2_out, extend_immed_in, extend_immed_out, funct_in, funct_out, rt_in, rt_out, rd_in, rd_out, 
-        RegDst_in, RegDst_out, ALUSrc_in, ALUSrc_out, MemtoReg_in, MemtoReg_out, RegWrite_in, RegWrite_out, MemRead_in, MemRead_out, MemWrite_in, MemWrite_out,
-        ALUOp_in, ALUOp_out ) ;
+module  ID_EX (reset, clk, RD1_in, RD1_out, RD2_in, RD2_out, extend_immed_in, extend_immed_out,
+               funct_in, funct_out, rt_in, rt_out, rd_in, rd_out, RegDst_in, RegDst_out, 
+               ALUSrc_in, ALUSrc_out, MemtoReg_in, MemtoReg_out, RegWrite_in, RegWrite_out,
+               MemRead_in, MemRead_out, MemWrite_in, MemWrite_out, ALUOp_in, ALUOp_out) ;
 
 input clk, reset, RegDst_in, ALUSrc_in, MemtoReg_in, RegWrite_in, MemRead_in, MemWrite_in ;
 input [31:0] RD1_in, RD2_in, extend_immed_in ;
 input [5:0] funct_in ;
 input [4:0] rt_in, rd_in ;
 input [1:0] ALUOp_in ;
-
 
 output [31:0] RD1_out, RD2_out, extend_immed_out ;
 output [5:0] funct_out ;
@@ -22,7 +22,7 @@ reg [1:0] ALUOp_out ;
 reg RegDst_out, ALUSrc_out, MemtoReg_out, RegWrite_out, MemRead_out, MemWrite_out ;
 
 always @(posedge clk) begin
-    if ( reset ) begin
+    if (reset) begin
         RD1_out <= 32'b0 ;
         RD2_out <= 32'b0 ;
         extend_immed_out <= 32'b0 ;
@@ -52,5 +52,4 @@ always @(posedge clk) begin
         MemWrite_out <= MemWrite_in ;
     end
 end
-
 endmodule
