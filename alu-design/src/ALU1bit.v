b241078ef9aee6a6b5a5101bf097bd23e6142210
MacWillyLiao <116270282+MacWillyLiao@users.noreply.github.com>
2026-01-17 12:19:32 +0800

    Update ALU1bit.v

--- alu-design/src/ALU1bit.v
+++ alu-design/src/ALU1bit.v
@@ -1,22 +1,19 @@
-module ALU1bit
-(a,b,less,inv,cin,cout,sel,result) ;
+module ALU1bit (a, b, less, inv, cin, cout, sel, result);
   
-  input a,b,inv,less,cin ;
+  input a, b, inv, less, cin;
   input [1:0] sel;
-  output result ;
-  output cout ;
+  output result;
+  output cout;
   
-  wire e1,e2,e3,sum ;
-  and(e1,a,b);
-  or(e2,a,b);
-  xor(e3,b,inv);
+  wire e1, e2, e3, sum;
+  and (e1, a, b);
+  or (e2, a, b);
+  xor (e3, b, inv);
   
-  FA fa(.a(a),.b(e3),.cin(cin),.cout(cout),.sum(sum));
+  FA fa (.a(a), .b(e3), .cin(cin), .cout(cout), .sum(sum));
   
   // mux
-
-  assign result = (sel[1]) ? ( sel[0] ? less : sum ) : (sel[0] ? e2 : e1 ) ;  
-
-endmodule    
-     
   
+  assign result = (sel[1]) ? (sel[0] ? less : sum) : (sel[0] ? e2 : e1);  
+
+endmodule
