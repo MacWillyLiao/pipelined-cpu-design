6fa66d2d9d38b17a894d9733037d27bd99c7a8ff
MacWillyLiao <116270282+MacWillyLiao@users.noreply.github.com>
2026-01-18 16:26:21 +0800

    Update tb_ALU.v

--- alu-design/src/tb_ALU.v
+++ alu-design/src/tb_ALU.v
@@ -1,10 +1,11 @@
-`timescale 1ns/ 1ns
-module tb_ALU();
+`timescale 1ns/1ns
+module tb_ALU ();
+	
 	reg clk, rst;
-	reg[5:0] ctrl;
-	reg[31:0] inputA, inputB;
+	reg [5:0] ctrl;
+	reg [31:0] inputA, inputB;
 	reg [31:0] ans;
-	wire[31:0] out;
+	wire [31:0] out;
 	integer fp_r, fp_r_ans, eof;
 	
 	initial begin
@@ -18,60 +19,60 @@ module tb_ALU();
 		#10;
 		rst = 1'b0;
 
-		fp_r = $fopen( "input.txt", "r" );
-		fp_r_ans = $fopen( "ans.txt", "r" );
-		$display( "Start\n" );
+		fp_r = $fopen("input.txt", "r");
+		fp_r_ans = $fopen("ans.txt", "r");
+		$display("Start\n");
 		eof = $fscanf(fp_r_ans, "%d", ans);
-		while( eof != -1 ) begin
-			$fscanf(fp_r, "%d%d%d", ctrl, inputA, inputB );
-			$write( "%d: Input: ", $time/10 );
-			if ( ctrl == 6'd36 ) $write( "AND(%d)", ctrl );
-			else if ( ctrl == 6'd37 ) $write( "OR(%d) ", ctrl );
-			else if ( ctrl == 6'd32 ) $write( "ADD(%d) ", ctrl );
-			else if ( ctrl == 6'd34 ) $write( "SUB(%d) ", ctrl );
-			else if ( ctrl == 6'd42 ) $write( "SLT(%d) ", ctrl );
-			else if ( ctrl == 6'd00 ) $write( "SLL(%d) ", ctrl );
-			else if ( ctrl == 6'd25 ) $write( "MULTU(%d) ", ctrl );
-			$display( "%d%d", inputA, inputB  );
-			if ( ctrl == 6'd25 ) begin
+		while(eof != -1) begin
+			$fscanf(fp_r, "%d%d%d", ctrl, inputA, inputB);
+			$write("%d: Input: ", $time/10);
+			if (ctrl == 6'd36) $write("AND(%d)", ctrl);
+			else if (ctrl == 6'd37) $write("OR(%d) ", ctrl);
+			else if (ctrl == 6'd32) $write("ADD(%d) ", ctrl);
+			else if (ctrl == 6'd34) $write("SUB(%d) ", ctrl);
+			else if (ctrl == 6'd42) $write("SLT(%d) ", ctrl);
+			else if (ctrl == 6'd00) $write("SLL(%d) ", ctrl);
+			else if (ctrl == 6'd25) $write("MULTU(%d) ", ctrl);
+			$display("%d%d", inputA, inputB);
+			if (ctrl == 6'd25) begin
 				#330;
-				$display( "%d: Mul End\n", $time/10 );
+				$display("%d: Mul End\n", $time/10);
 				
-				$display( "                   Move Hi" );
+				$display("                   Move Hi");
 				ctrl = 6'd16;
 				#10;
-				if ( ans == out )
-					$display( "%d: Correct: Your answer is:%d,\n                                 Correct answer is:%d\n", $time/10, out, ans );
+				if (ans == out)
+					$display("%d: Correct: Your answer is:%d,\n                                 Correct answer is:%d\n", $time/10, out, ans);
 				else
-					$display( "%d: Wrong Answer: Your answer is:%d,\n                                             Correct answer is:%d\n", $time/10, out, ans );
-				$display( "                   Move Lo" );
+					$display("%d: Wrong Answer: Your answer is:%d,\n                                             Correct answer is:%d\n", $time/10, out, ans);
+				$display("                   Move Lo");
 				ctrl = 6'd18;
 				eof = $fscanf(fp_r_ans, "%d", ans);
 				#10;
-				if ( ans == out )
-					$display( "%d: Correct: Your answer is:%d,\n                                 Correct answer is:%d\n", $time/10, out, ans );
+				if (ans == out)
+					$display("%d: Correct: Your answer is:%d,\n                                 Correct answer is:%d\n", $time/10, out, ans);
 				else
-					$display( "%d: Wrong Answer: Your answer is:%d,\n                                             Correct answer is:%d\n", $time/10, out, ans );
+					$display("%d: Wrong Answer: Your answer is:%d,\n                                             Correct answer is:%d\n", $time/10, out, ans);
 			end
 			else begin
 				#10;
-				if ( ans == out )
-					$display( "%d: Correct: Your answer is:%d,\n                                 Correct answer is:%d\n", $time/10, out, ans );
+				if (ans == out)
+					$display("%d: Correct: Your answer is:%d,\n                                 Correct answer is:%d\n", $time/10, out, ans);
 				else
-					$display( "%d: Wrong Answer: Your answer is:%d,\n                                             Correct answer is:%d\n", $time/10, out, ans );
+					$display("%d: Wrong Answer: Your answer is:%d,\n                                             Correct answer is:%d\n", $time/10, out, ans);
 			end
 			eof = $fscanf(fp_r_ans, "%d", ans);
 			rst = 1'b1;
 		  #10;
 		  rst = 1'b0;
 		end
-		$fclose( fp_r );
-		$fclose( fp_r_ans );
-		$display( "Simulation End\n" );
+		$fclose(fp_r);
+		$fclose(fp_r_ans);
+		$display("Simulation End\n");
 		$stop();
 	end
 
-	TotalALU alu( .clk(clk), .reset(rst), .dataA(inputA), .dataB(inputB), .Signal(ctrl), .Output(out) ) ;
+	TotalALU alu(.clk(clk), .reset(rst), .dataA(inputA), .dataB(inputB), .Signal(ctrl), .Output(out));
 
 endmodule 
 
